7520d9542fd48cf85d3c8bb5e3d12bce52f32a23
Dmytro Mykhailov <dmytro.mykhailov@zivver.com>
2021-03-04 12:07:08 +0100

    Adding basic startup script for the instance.

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,6 +93,7 @@ resource "aws_instance" "performance_test_1" {
   key_name        = "MyDefaultKeyPair"
   security_groups = [aws_security_group.ingress_ssh.id]
   subnet_id       = aws_subnet.subnet-uno.id
+  user_data = file("configure_instance.sh")
 }
 
 resource "aws_internet_gateway" "test-env-gw" {
